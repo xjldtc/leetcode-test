2a95a31894e2c5722a68eecf47fd645ad4697bec
xjldtc <xujl@newlandcomputer.com>
2018-07-30 19:46:35 +0800

    优化sumNumMinus方法

--- src/com/xjldtc/leetcode/questionbank1/TwoNumSum.java
+++ src/com/xjldtc/leetcode/questionbank1/TwoNumSum.java
@@ -10,42 +10,34 @@ public class TwoNumSum {
 		for (int i = 0; i < bigNums.length; i++) {
 			bigNums[i] = i;
 		}
-		bigNums = new int[]{3,2,4};
 		System.out.println(System.currentTimeMillis());
-		int[] retrunNums = TwoNumSum.sumNum(bigNums, 6);
+		int[] retrunNums = TwoNumSum.sumNumMinus(bigNums, 195152);
 		System.out.println(retrunNums[0] + "," + retrunNums[1]);
 		System.out.println(System.currentTimeMillis());
-		int[] retrunNums2 = TwoNumSum.hashSumNum(bigNums, 195152);
+		int[] retrunNums2 = TwoNumSum.sumNumAdd(bigNums, 195152);
 		System.out.println(retrunNums2[0] + "," + retrunNums2[1]);
 		System.out.println(System.currentTimeMillis());
-		int[] retrunNums3 = TwoNumSum.oneHashSumNum(bigNums, 195152);
-		System.out.println(retrunNums3[0] + "," + retrunNums3[1]);
-		System.out.println(System.currentTimeMillis());	
 	}
 
 	/**
 	 * 暴力法 时间复杂度：O(n^2)， 对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素， 这将耗费 O(n)
 	 * 的时间。因此时间复杂度为 O(n^2)。 空间复杂度：O(1).
+	 * add 与 sumNumMinus不存在时间不一样
 	 */
-	public static int[] sumNum(int[] numbers, int target) {
-		int[] retrunNums = new int[2];
+	public static int[] sumNumMinus(int[] numbers, int target) {
 		for (int i = 0; i < numbers.length; i++) {
-			int a = numbers[i];
-			for (int j = numbers.length - 1; j > i - 1; j--) {// 注意考虑数组是0开始，总数是实际数量
-				int b = numbers[j];
-				if (target == (a + b)&&i!=j) {
-					retrunNums[0] = i;
-					retrunNums[1] = j;
-					return retrunNums;
+			for (int j = numbers.length - 1; j > i ; j--) {// 注意考虑数组是0开始，总数是实际数量
+				if ( target == (numbers[i] + numbers[j])) {
+					return new int[] { i, j };
 				}
 			}
 		}
 		throw new IllegalArgumentException("No two sum solution");
 
 		// 使用减法 循环就用加法
-		// for (int i = 0; i < nums.length; i++) {
-		// for (int j = i + 1; j < nums.length; j++) {
-		// if (nums[j] == target - nums[i]) {
+		// for (int i = 0; i < numbers.length; i++) {
+		// for (int j = i + 1; j < numbers.length; j++) {
+		// if (numbers[j] == target - numbers[i]) {
 		// return new int[] { i, j };
 		// }
 		// }
@@ -54,9 +46,25 @@ public class TwoNumSum {
 	}
 
 	/**
-	 * 利用hash必须保证值不一样，否则会出问题例如{3，3} 6 会返回不了结果
-	 * 时间复杂度：O(n)， 我们把包含有 n 个元素的列表遍历两次.由于哈希表将查找时间缩短到 O(1),所以时间复杂度为 O(n)
-	 * 空间复杂度：O(n)， 所需的额外空间取决于哈希表中存储的元素数量，该表中存储了 n个元素。
+	 * 暴力法 时间复杂度：O(n^2)， 对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素， 这将耗费 O(n)
+	 * 的时间。因此时间复杂度为 O(n^2)。 空间复杂度：O(1).
+	 */
+	public static int[] sumNumAdd(int[] numbers, int target) {
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[j] == target - numbers[i]) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+
+	}
+
+	/**
+	 * 利用hash必须保证值不一样，否则会出问题例如{3，3} 6 会返回不了结果 时间复杂度：O(n)， 我们把包含有 n
+	 * 个元素的列表遍历两次.由于哈希表将查找时间缩短到 O(1),所以时间复杂度为 O(n) 空间复杂度：O(n)，
+	 * 所需的额外空间取决于哈希表中存储的元素数量，该表中存储了 n个元素。
 	 */
 	public static int[] hashSumNum(int[] numbers, int target) {
 		Map<Integer, Integer> makeMap = new HashMap<Integer, Integer>();
@@ -73,8 +81,8 @@ public class TwoNumSum {
 	}
 
 	/**
-	 * 时间复杂度：O(n)， 我们只遍历了包含有 n 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
-	 * 空间复杂度：O(n)， 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 n个元素。
+	 * 时间复杂度：O(n)， 我们只遍历了包含有 n 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。 空间复杂度：O(n)，
+	 * 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 n个元素。
 	 */
 	public static int[] oneHashSumNum(int[] numbers, int target) {
 		Map<Integer, Integer> makeMap = new HashMap<Integer, Integer>();
